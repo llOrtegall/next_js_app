daa50e2648420d97a9852d9f69f53e098a1efcff
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-12 11:57:08 -0500

    termiando

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require 'flight/Flight.php';  /* framework */
 //*TODO: usando el framework de flight la función register hace la conexción a la base de datos 
 //*? db = user base de datos, 'PDO' = tipo de conexción de php y dentro de un array enviamos los parametros de la base de datos tales como direccion ip nombre de la base de datos y el usuario con la contraseña.
 Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=my_user_db', 'root', ''));
+
 //**LEER LOS DATOS Y LOS MUESTRA A CUALQUIER INTERFAZ */
 //*?Flight router ----> Bajo escena este metodo simplifica las propiedades que se colocan generalmente a la hora de realizar peticiones a las bases de datos ahorra mucho codigo php pero por debajo realiza todas esas peticiones con sus metodos y susparametros */
 Flight::route('GET /users', function () {
@@ -12,6 +13,7 @@ Flight::route('GET /users', function () {
   $datos = $sentencia->fetchAll(); //*? devuelve todos los datos con la instrucción fetchALL*/
   Flight::json($datos); //*? pasamos datos a un formato JSON el cual facilita la lectura de los mismos//
 });
+
 //*Recepciona los datos por método POST y hace una incerpción a la base de datos*/
 //TODO:  'NAMES': 'Ivan'
 //TODO:  'LASTNAMES': 'Ortega'
