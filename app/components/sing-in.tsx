8debe8bfdce50d332b4b076f5f26efa343dd12f7
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-15 10:40:35 -0500

    google ok antes de implementar adapters

--- app/components/sing-in.tsx
+++ app/components/sing-in.tsx
@@ -1,15 +1,26 @@
 
 import { signIn } from "@/lib/auth"
- 
+
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("github")
-      }}
-    >
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">Signin with GitHub</button>
-    </form>
+    <>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">Signin with GitHub</button>
+      </form>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+      >
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">Signin with Google</button>
+      </form>
+    </>
+
   )
 } 
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,15 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub({
     clientId: process.env.AUTH_GITHUB_ID ?? "",
     clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
-  })],
+  }),
+  Google({
+    clientId: process.env.AUTH_GOOGLE_ID ?? "",
+    clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+  }),
+],
 })
