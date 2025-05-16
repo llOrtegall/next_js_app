659afcfa694738febfaeff84e46e27f377a2dc2e
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-16 11:12:57 -0500

    auth js ok and works on modo dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import SignOut from "@/components/auth/sing-out";
+import SignIn from "@/components/auth/sing-in";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
   return (
     <div className="p-2">
-      <Button>Button</Button>
+      {session ? (
+        <div>
+          <p>Logged in as {session.user?.email}</p>
+          <SignOut />
+        </div>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 }
